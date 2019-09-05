2b55c2c2afa85580d7d440f233490f3510805f10
MELFI PEREZ <perezmelfi@outlook.com>
2019-09-05 10:19:12 -0700

    testing memeberships

---
+++ Omnibus/src/test/java/com/DeweyDecimate/Omnibus/models/ClubDiscussionTest.java
@@ -0,0 +1,24 @@
+package com.DeweyDecimate.Omnibus.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClubDiscussionTest {
+
+    @Test
+    public void testGetTimeStamp() {
+    }
+
+    @Test
+    public void testGetContent() {
+    }
+
+    @Test
+    public void testGetDiscussionUser() {
+    }
+
+    @Test
+    public void testGetBookClub() {
+    }
+}
